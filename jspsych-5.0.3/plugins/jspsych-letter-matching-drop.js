a64f42f0e2e44e18d0bf88e64c5c63ad57746dd4
N.B <niburka@vassar.edu>
2017-01-04 17:39:05 -0500

    instructions, practice, post-test, minor fixes

--- jspsych-5.0.3/plugins/jspsych-letter-matching-drop.js
+++ jspsych-5.0.3/plugins/jspsych-letter-matching-drop.js
@@ -34,14 +34,18 @@
 			var border_size = trial.square_size / 12;
 			var letter_size = trial.square_size / 2;
 			var top_letter = inner_window/2 - letter_size/2;
-			var half = inner_window - (letter_size + border_size);
-			var o = top_letter - (letter_size*2);
+
+
+			var right_letter_x = inner_window - (letter_size + border_size*2);
+			var right_letter_y = top_letter - (letter_size*2);
 			var center_inner_window = inner_window - letter_size;
 			var letbor = (letter_size+border_size);
 
+			var hei = trial.square_size*2;
+
 			// display stimulus
 			display_element.html(
-			'<div id="jspsych-letter-matching-container" style="position: relative; margin:auto; height: '+trial.square_size*2+'px; width:'+(2*(inner_window+letter_size)+trial.square_size-border_size)+'px;overflow:hidden;">'+
+			'<div id="jspsych-letter-matching-container" style="position: relative; margin:auto; height: '+hei+'px; width:'+(2*(inner_window+letter_size)+trial.square_size-border_size)+'px;overflow:hidden;">'+
 			'<div id="jspsych-letter-matching-left" style="position: absolute; top:'+border_size+'px;left:0px;width:'+inner_window+'px; height: '+inner_window+'px;">'+
 				'<div id="jspsych-letter-matching-stim-left" style="font-family: \'Roboto Mono\'; font-size:'+letter_size+'px; width:'+letter_size+'px;height:'+letter_size+'px; line-height: '+letter_size+'px; text-align:center; position:absolute; top:'+top_letter+'px; left: '+center_inner_window+'px;"><span>'+trial.prev_two_item+'</span></div>'+
 			'</div>'+
@@ -50,7 +54,7 @@
 			'</div>'+
 			'<div id="jspsych-letter-matching-right" style="position: absolute;top:0px;left:'+(2*(inner_window+letter_size)-border_size)+'px; width:'+trial.square_size+'px; height:'+trial.square_size+'px;">'+
         '<div id="jspsych-letter-matching-inner" style="width:'+inner_window+'px; height: '+inner_window+'px; position: relative; border: solid #ddd '+border_size+'px; background-color: #ddd;">'+
-          '<div id="jspsych-letter-matching-stim-right" style="font-family: \'Roboto Mono\'; font-size:'+letter_size+'px; width:'+letter_size+'px;height:'+letter_size+'px; line-height: '+letter_size+'px; text-align:center; position:absolute; top:'+ o +'px; left: '+ half +'px;"><span>'+trial.item+'</span></div>'+
+          '<div id="jspsych-letter-matching-stim-right" style="font-family: \'Roboto Mono\'; font-size:'+letter_size+'px; width:'+letter_size+'px;height:'+letter_size+'px; line-height: '+letter_size+'px; text-align:center; position:absolute; top:'+ right_letter_y +'px; left: '+ right_letter_x +'px;"><span>'+trial.item+'</span></div>'+
         '</div>'+
       '</div>'+
 			'</div>');
@@ -101,16 +105,16 @@
 				$('#jspsych-letter-matching-stim-right').css({top:top_letter, marginTop:0});
 
 				//swing the letters to the left
-				$('#jspsych-letter-matching-stim-left').animate({left:-letter_size*2+"px"},trial.timing_duration,"swing");
-				$('#jspsych-letter-matching-stim-middle').animate({left:-letter_size*2+"px"},trial.timing_duration,"swing");
-				$('#jspsych-letter-matching-stim-right').animate({left:-letter_size*2+"px"},trial.timing_duration,"swing");
+				$('#jspsych-letter-matching-stim-left').animate({left:-trial.square_size+"px"},trial.timing_duration,"swing");
+				$('#jspsych-letter-matching-stim-middle').animate({left:-trial.square_size+"px"},trial.timing_duration,"swing");
+				$('#jspsych-letter-matching-stim-right').animate({left:-trial.square_size+"px"},trial.timing_duration,"swing");//({left:-letter_size*2+"px"},trial.timing_duration,"swing");
 	
 
 				response.key = info.key;
 				response.rt = info.rt;
 				response.correct = correct;
 
-				//end the trial after the animation has run
+				//end the trial after the animation has run 
 				setTimeout(function() {
 					end_trial();
 				}, trial.timing_duration);
